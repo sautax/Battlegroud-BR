925ae54ba5f7500e7532c8702691f668ba304ac4
sautax <sautaxgaming@gmail.com>
2018-02-18 22:29:25 +0100

    setting up server

--- bin/server/index.js
+++ bin/server/index.js
@@ -1,18 +1,40 @@
-
+let http = require("http")
 let ejs = require("ejs");
-let express = require("express")
-let app = express()
-
-app.get("/",function(req,res){
-    res.render("index.ejs")
+let fs = require("fs") 
+let url = require("url");
+var server = http.createServer(function(req, res) {
+    let page = url.parse(req.url).pathname;
+    let arrayS = Array.from(page)
+    let three = ""
+    for (let i = 0; i < 4; i++){
+        three=three +arrayS[i]
+    }
+    if (page === "/"){
+        fs.readFile('../../index.html', 'utf-8', function(error, content) {
+            res.writeHead(200, {"Content-Type": "text/html"});
+            res.end(content);
+        });
+    }
+    else if (three ==="/bin") {
+        fs.readFile('../..'+page, 'utf-8', function(error, content) {
+            if (!error){
+                res.writeHead(200, {"Content-Type": "text/javascript"});
+                res.end(content);
+            }
+            else{
+                res.writeHead(404)
+                res.end()
+            }
+        });
+    }
 });
-app.use(express.static(__dirname + '../../../'));
-let io = require('socket.io').listen();
+
+let io = require('socket.io').listen(server);
 io.sockets.on('connection', function (socket) {
-
-    console.log('client connected');
-
+    console.log('client connected');  
 });
+//app.use(express.static(__dirname + '../../../'));
 
 
-app.listen(8080)
+server.listen(8080)
+console.log("listening on 8080")
