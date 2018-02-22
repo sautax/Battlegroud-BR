eb706bdefb880e54d70b22a53405a524f9ac41db
sautax <sautaxgaming@gmail.com>
2018-02-22 22:51:04 +0100

    base server working now

--- bin/client/index.js
+++ bin/client/index.js
@@ -18,7 +18,9 @@ document.addEventListener("keydown", function (e) {
     if (e.key === "ArrowDown") {
         down = 1;
     }
-
+    vect[1] = 50 * down - 50 * up;
+    vect[0] = 50 * right - 50 * left;
+    socket.emit("move",vect)
 });
 document.addEventListener("keyup", function (e) {
     if (e.key === "ArrowLeft") {
@@ -33,6 +35,9 @@ document.addEventListener("keyup", function (e) {
     if (e.key === "ArrowDown") {
         down = 0;
     }
+    vect[1] = 50 * down - 50 * up;
+    vect[0] = 50 * right - 50 * left;
+    socket.emit("move",vect)
 });
 let requestAnimFrame = (function () {
     return window.requestAnimationFrame ||
@@ -40,6 +45,7 @@ let requestAnimFrame = (function () {
             window.setTimeout(callback, 1000 / 60);
         };
 })();
+let vect = []
 let mouseX;
 let mouseY;
 let right = 0;
@@ -47,42 +53,36 @@ let left = 0;
 let up = 0;
 let down = 0;
 let pseudo = prompt('enter a pseudo');
+let players = []
+let clientPlayer
 socket.emit('newP', pseudo);
-socket.on("Pupdate", function (cp) {
+socket.on("update", function (ps) {
+    players = ps
+})
+socket.on("Cupdate", function (Cplayer) {
+    clientPlayer = Cplayer
+})
+socket.on("Pupdate", function (cp, mp) {
     let canvas = document.querySelector("#canvas");
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     let context = canvas.getContext('2d');
-    let clientPlayer = cp
+    clientPlayer = cp
     mouseX = canvas.width / 2;
     mouseY = canvas.height / 2;
-    let vect = [0, 0];
-    let players = [];
     let x;
     let y;
-    let m = new map(5000, 5000);
+    let m = mp
     let scl = canvas.width / 256;
-    for (let i = 0; i < 50; i++) {
-        x = Math.floor(Math.random() * 4900);
-        y = Math.floor(Math.random() * 4900);
-        players.push(new player([x, y], "red"));
-    }
+    let step = 0
 
     function draw() {
+
         // vect[0] = mouseX - canvas.width/2
         // vect[1] = mouseY - canvas.height/2
-        vect[1] = 50 * down - 50 * up;
-        vect[0] = 50 * right - 50 * left;
-        vect[0] *= scl;
-        vect[1] *= scl;
-        if (clientPlayer.x + vect[0] / 8 > -scl && clientPlayer.x + vect[0] / 8 < m.width * scl + scl - canvas.width / 8) {
-            clientPlayer.x += vect[0] / 8;
-        }
-        if (clientPlayer.y + vect[1] / 8 > -scl && clientPlayer.y + vect[1] / 8 < m.height * scl + scl - canvas.width / 8) {
-            clientPlayer.y += vect[1] / 8;
-        }
-        clientPlayer.Wangle = Math.atan((mouseY - canvas.height / 2) / (mouseX - canvas.width / 2));
-        console.log(clientPlayer.Wangle);
+
+        
+        // clientPlayer.Wangle = Math.atan((mouseY - canvas.height / 2) / (mouseX - canvas.width / 2));
         context.fillStyle = "#709e9c";
         context.fillRect(0, 0, canvas.width, canvas.height);
         context.save();
@@ -92,6 +92,7 @@ socket.on("Pupdate", function (cp) {
         context.fillStyle = clientPlayer.color;
         context.fillRect(-canvas.width / 16, -canvas.width / 16, canvas.width / 8, canvas.width / 8);
         context.translate(-(clientPlayer.x + canvas.width / 16), -(clientPlayer.y + canvas.width / 16));
+        console.log(players)
         for (let i = 0; i < players.length; i++) {
             context.fillStyle = players[i].color;
             context.fillRect(players[i].x * scl, players[i].y * scl, canvas.width / 8, canvas.width / 8);
--- bin/engine/index.js
+++ bin/engine/index.js
@@ -1,12 +1,13 @@
 "use strict";
 //engine
 class player {
-    constructor(pos, color, pseudo) {
+    constructor(pos, color, pseudo, id) {
         this.x = pos[0];
         this.y = pos[1];
         this.Wangle = 0;
         this.color = color;
         this.pseudo = pseudo
+        this.id = id
     }
 }
 class map {
