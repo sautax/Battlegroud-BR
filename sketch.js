c476b54101c61e8057e055cdbcfd94d0846ab3f0
sautax <sautaxgaming@gmail.com>
2018-02-24 21:56:38 +0100

    some tests of p5

--- sketch.js
+++ sketch.js
@@ -1,7 +1,15 @@
+var rects = []
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth, windowHeight);
+  for(let i = 0;i<50;i++){
+    rects.push([[random(width),random(height)],[random(255),random(255),random(255)]])
+  }
 }
 
 function draw() {
-  // put drawing code here
+  background("#80ffff");
+  for(let i = 0;i<50;i++){
+    fill(rects[i][1][0],rects[i][1][1],rects[i][1][2])
+    rect(rects[i][0][0],rects[i][0][1],35,35,5)
+  }
 }
