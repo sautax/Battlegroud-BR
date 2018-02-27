dab7837a501daf8561653e031495a1f9a7e9501e
sautax <sautaxgaming@gmail.com>
2018-02-27 19:48:21 +0100

    bug fix

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 var rects = []
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth-4, windowHeight-4);
   for(let i = 0;i<50;i++){
     rects.push([[random(width),random(height)],[random(255),random(255),random(255)]])
   }
@@ -11,5 +11,12 @@ function draw() {
   for(let i = 0;i<50;i++){
     fill(rects[i][1][0],rects[i][1][1],rects[i][1][2])
     rect(rects[i][0][0],rects[i][0][1],35,35,5)
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  for(let i = 0;i<50;i++){
+    rects[i] = [[random(width),random(height)],[random(255),random(255),random(255)]]
   }
 }
